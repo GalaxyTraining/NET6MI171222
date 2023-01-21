d1e56aeee2ebc59fa22c4c6c550b40651d8aa8e3
Anthony Jesus Portilla Cano <aportilla@externo.auna.pe>
2023-01-21 18:14:57 -0500

    Se sube los cambios de la clase04

---
+++ Demo/DemoClase04/Clase04SP/Program.cs
@@ -0,0 +1,29 @@
+﻿
+using System.Data;
+using System.Data.SqlClient;
+
+string connectionString = "data source = DESKTOP-0RI6A0M\\MSSQLSERVER01; initial catalog = DbTest; user id = sa; password = 12345;";
+string SQL = "SP_INSERT_PRODUCTO";
+
+try
+{
+    using SqlConnection con = new(connectionString);
+    using SqlCommand cmd = new(SQL, con);
+    cmd.CommandType = CommandType.StoredProcedure;
+
+    SqlParameter param;
+    param = cmd.Parameters.Add("@Nombre", SqlDbType.VarChar, 100);
+    param.Value = "Todino";
+    param = cmd.Parameters.Add("@Tipo", SqlDbType.VarChar, 100);
+    param.Value = "Paneton";
+    param = cmd.Parameters.Add("@Precio", SqlDbType.Decimal);
+    param.Value = 25;
+    con.Open();
+    int rowsAffected = cmd.ExecuteNonQuery();
+    con.Close();
+}
+catch (Exception ex)
+{
+
+	throw;
+}
